d83fbfbf056137dcb2c7c50ecac7c52fe573680d
paprikaMarusan <m01162233e@edu.teu.ac.jp>
2018-11-14 18:27:21 +0900

    commit1

--- js/function181107.js
+++ js/function181107.js
@@ -9,7 +9,6 @@
 //   fruitsHTML += `<li>${fruitNames[i]}</li>`;
 //   i++;
 // }
-
 // for版
 // for(let i = 0;i < fruitNames.length;i++){
 //   fruitsHTML += `<li>${fruitNames[i]}</li>`;
@@ -21,16 +20,37 @@
 
 /*演習２：配列のデータを使うやつツイッタ風*/
 // let tweetsHTML = "<ul>";
-
 // let i = 0;
 // while(i < tweets.length){
 //   tweetsHTML += `<li><b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i></li>`;
 //   i++;
 // }
-
 // for(let i = 0; i < tweets.length;i++){
 //   tweetsHTML += `<li><b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i></li>`;
 // }
 //
 // tweetsHTML += "</ul>";
 // document.querySelector("#answer").innerHTML = tweetsHTML;
+
+/*演習3 Array#forEach*/
+
+/*フルーツのやつ*/
+// let fruitsHTML = "<ul>";
+// for版(Array #forEach阪)
+// let appendFruit = function(fruit/*配列のなかの文字列が引数？*/){
+//   fruitsHTML += `<li>${fruit/*配列の何番目かを考えなくていい？*/}</li>`;
+// }
+// fruitNames.forEach(appendFruit);
+// fruitsHTML += "</ul>";
+// document.querySelector("#answer").innerHTML = fruitsHTML;
+
+/*ツイートのやつ*/
+let tweetsHTML = "<ul>";
+
+let appendTweet = function (twit/*配列の中身*/) {
+ tweetsHTML += `<li>${twit.name} ${twit.message} ${twit.tweetedAt}</li>`;
+}
+tweets.forEach(appendTweet);
+
+tweetsHTML += "</ul>";
+document.querySelector("#answer").innerHTML = tweetsHTML;
