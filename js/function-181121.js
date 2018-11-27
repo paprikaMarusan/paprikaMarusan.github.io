f82dbc5ec6e3406b85ddfba3ee9a6ad4958f96bd
paprikaMarusan <m01162233e@edu.teu.ac.jp>
2018-11-27 11:46:26 +0900

    commit1

---
+++ js/function-181121.js
@@ -0,0 +1,19 @@
+fetch("https://sheets.googleapis.com/v4/spreadsheets/1ExGxj07uwHxG2oINmWPZsh69vEWl3nKRKHqwO8xrlAM/values/sheet1?key=AIzaSyAkzsT5GrlIb8AitV1Dwg-33H7uxFDfQ_g").then(
+    response => {
+      return response.json();
+    }
+).then(
+    json => {
+      let html = "<ul>";
+      console.log(json);
+
+      json.values.forEach( row => {
+            html += `<li>${row[0]}</li>`;
+          }
+      );
+
+      html += "</ul>";
+      document.querySelector("#result").innerHTML = html;
+    }
+);
+
--- js/tweetTest.js
+++ js/tweetTest.js
@@ -1,12 +1,56 @@
-/*ツイート*/
-let tweetsHTML = "<ul>";let number = 0;
+/*中間課題*/
+let tweetsHTML = "<ul>";
 
+//最初のページ
 let appendTweet = function (twit/*配列の中身*/) {
   tweetsHTML += `<li> <div class="imageBox"><img src = ${twit.avatar} width="100%"></div> <div class="textBox"> <b>${twit.name}</b> <br> ${twit.message} <br> <i>${twit.tweetedAt}</i></div></li>`;
 };
-
 tweets.forEach(appendTweet);
-
 
 tweetsHTML += "</ul>";
 document.querySelector("#answer").innerHTML = tweetsHTML;
+
+
+//ボタンが押されたらanswerの中身を書き換えるやつ
+// function saburo(){
+//   document.querySelector("#answer").innerHTML = "三郎召喚〜";
+//   //書き換える場所の取得.innnerHTML = 書き換える内容
+//   //必要なこと：三郎だけを取り出す
+//}
+
+//三郎だけ書き換えるやつ
+function saburo() {
+  let tweetsHTML = "<ul>";
+  let appendTweet = function (twit/*配列の中身*/) {
+    if (twit.name === "三郎BOT") {
+      tweetsHTML += `<li> <div class="imageBox"><img src = ${twit.avatar} width="100%"></div> <div class="textBox"> <b>${twit.name}</b> <br> ${twit.message} <br> <i>${twit.tweetedAt}</i></div></li>`;
+    }
+  };
+  tweets.forEach(appendTweet);
+  tweetsHTML += "</ul>";
+  document.querySelector("#answer").innerHTML = tweetsHTML;
+}
+
+//太郎だけ書き換えるやつ
+function taro() {
+  let tweetsHTML = "<ul>";
+  let appendTweet = function (twit/*配列の中身*/) {
+    if (twit.name === "太郎") {
+      tweetsHTML += `<li> <div class="imageBox"><img src = ${twit.avatar} width="100%"></div> <div class="textBox"> <b>${twit.name}</b> <br> ${twit.message} <br> <i>${twit.tweetedAt}</i></div></li>`;
+    }
+  };
+  tweets.forEach(appendTweet);
+  tweetsHTML += "</ul>";
+  document.querySelector("#answer").innerHTML = tweetsHTML;
+}
+function jiro() {
+  let tweetsHTML = "<ul>";
+  let appendTweet = function (twit/*配列の中身*/) {
+    if (twit.name === "次郎") {
+      tweetsHTML += `<li> <div class="imageBox"><img src = ${twit.avatar} width="100%"></div> <div class="textBox"> <b>${twit.name}</b> <br> ${twit.message} <br> <i>${twit.tweetedAt}</i></div></li>`;
+    }
+  };
+  tweets.forEach(appendTweet);
+  tweetsHTML += "</ul>";
+  document.querySelector("#answer").innerHTML = tweetsHTML;
+}
