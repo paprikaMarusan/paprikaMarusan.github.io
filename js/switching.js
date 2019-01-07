edbebe913fe20351c57b046228cb4e68b4f33d5c
paprikaMarusan <m01162233e@edu.teu.ac.jp>
2019-01-07 16:36:01 +0900

    最終課題のcommit4

--- js/switching.js
+++ js/switching.js
@@ -6,24 +6,25 @@ function moveQ1(){
 }
 
 function moveQ2(){
-  document.querySelector('#question').innerHTML = `<h2>このサイトを作った人は誰ですか？</h2>`;
+  document.querySelector('#question').innerHTML = `<h2>Q.このサイトを作った人は誰ですか？</h2>`;
   document.querySelector('#answer').innerHTML = `<p>なんと右側のTwitterバナーから作者の<a href="https://twitter.com/kiiro_no_hoshi" target="_blank">Twitter</a>に飛べてしまうんです！</p>`;
   document.querySelector('.questionMove').innerHTML = `<li onclick="moveQ1()">&#9312;</li> <li onclick="moveQ2()">&#10103;</li> <li onclick="moveQ3()">&#9314;</li>`;
 }
 
 function moveQ3(){
-  document.querySelector('#question').innerHTML = `<h2>Githubとかありますか？</h2>`;
+  document.querySelector('#question').innerHTML = `<h2>Q.Githubとかありますか？</h2>`;
   document.querySelector('#answer').innerHTML = `<p>なんとここに<a href="https://github.com/paprikaMarusan" target="_blank">Github</a>へのリンクが...！</p>`;
   document.querySelector('.questionMove').innerHTML = `<li onclick="moveQ1()">&#9312;</li> <li onclick="moveQ2()">&#9313;</li> <li onclick="moveQ3()">&#10104;</li>`;
 }
 
 
 
+//キャラクターを切り替えるやつ
 function changeLuke(){
   document.querySelector('.character').innerHTML = `<img src="images/hirona_unit.png">`;
   document.querySelector('.characterName').innerHTML = `<h1><span>ルーク</span></h1>`;
   document.querySelector('.characterAbility').innerHTML = `<p><span>炎属性ファイター｜19歳｜カラクリ：フレイムナックル</span></p>`;
-  document.querySelector('.characterProfile').innerHTML = `<p><span>街を守る警備組織 [カラクリナイツ] の一員.<br>何事にも全力で情に厚い男.<br>同期のヒロナからは「熱血すぎ」と呆れられがち.<br>今日もカラクリ片手に街の平和を守り抜く！<br><br></span></p><p><span>「平和を乱すのはどこのどいつだ！」</span></p>`;
+  document.querySelector('.characterProfile').innerHTML = `<p><span>街を守る警備組織 [カラクリナイツ] の一員.<br>何事にも全力で情に厚い男.<br>同期のヒロナからは「熱血すぎ」と呆れられがち.<br>本人はそんなことを意に介さず、<br>今日もカラクリ片手に街の平和を守り抜く！<br><br></span></p><p><span>「街の平和を乱すのはどこのどいつだ！」</span></p>`;
   document.querySelector('.characterMove').innerHTML = `      <img src="images/mimil_charaparts_active.png" onClick="changeLuke()"><img src="images/hirona_charaparts.png" onClick="changeHirona()"><img src="images/mimil_charaparts.png" onClick="changeKana()"><img src="images/mimil_charaparts.png" onClick="changeMichelle()"><img src="images/mimil_charaparts.png" onClick="changeLeonard()">
 `;
 }
@@ -57,7 +58,7 @@ function changeLeonard(){
   document.querySelector('.character').innerHTML = `<img src="images/mimil_unit.png">`;
   document.querySelector('.characterName').innerHTML = `<h1><span>レオナルド</span></h1>`;
   document.querySelector('.characterAbility').innerHTML = `<p><span>闇属性ランサー｜20歳｜カラクリ：ランス</span></p>`;
-  document.querySelector('.characterProfile').innerHTML = `<p><span>ちょっぴり危ない雰囲気の女の子.<br>惚れっぽい性格で、いつも恋に燃えている.<br>カラクリアリーナに挑戦し始めてからは、<br>自分より強い人に惹かれるようになったらしい.<br><br></span></p><p><span>「あぁ、あの人こそきっと私の王子様！」</span></p>`;
+  document.querySelector('.characterProfile').innerHTML = `<p><span>ちょっぴり危ない雰囲気の女の子.<br>惚れっぽい性格で、いつも恋に燃えている.<br>カラクリアリーナに挑戦し始めてからは、<br>自分より強い人に惹かれるようになったらしい.<br><br></span></p><p><span>「」</span></p>`;
   document.querySelector('.characterMove').innerHTML = `      <img src="images/mimil_charaparts.png" onClick="changeLuke()"><img src="images/hirona_charaparts.png" onClick="changeHirona()"><img src="images/mimil_charaparts.png" onClick="changeKana()"><img src="images/mimil_charaparts.png" onClick="changeMichelle()"><img src="images/mimil_charaparts_active.png" onClick="changeLeonard()">
 `;
 }
