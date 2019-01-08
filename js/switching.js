e25e1fc011fa75501bfa32bb9a966f88b564ebe3
paprikaMarusan <m01162233e@edu.teu.ac.jp>
2019-01-08 23:58:04 +0900

    モバイル用：フォントサイズの調整

--- js/switching.js
+++ js/switching.js
@@ -1,6 +1,6 @@
 //質問を切り替えるやつ
 function moveQ1(){
-  document.querySelector('#question').innerHTML = `<h2>Q.カラクリウォーズって実際に配信されているんですか？</h2>`;
+  document.querySelector('#question').innerHTML = `<h2>Q.ゲームは実際に配信されてますか？</h2>`;
   document.querySelector('#answer').innerHTML = `<p>カラクリウォーズは実際に配信されていません.<br>このサイトは大学の演習で作られたものです.</p>`;
   document.querySelector('.questionMove').innerHTML = `<li onclick="moveQ1()">&#10102;</li> <li onclick="moveQ2()">&#9313;</li> <li onclick="moveQ3()">&#9314;</li>`;
 }
